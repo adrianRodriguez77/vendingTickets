0f7238792315f813448fb1aacab01e9c55daaca5
adrian [itb] rodriguez vegas <adrian.rodriguez.vegas.7e6@itb.cat>
2024-01-02 15:57:33 +0100

    Mejoras de menú y funcionamiento de control de errores

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,77 @@
 package org.example
 
+import java.util.Scanner
+
+var scanner = Scanner(System.`in`)
+
 fun menuTickets(){
     println("""
         MENÚ VENDING TICKETS (Metro, bus i tren)
+        
         1. Bitllet senzill
         2. TCasual
         3. TUsual
         4. TFamiliar
         5. TJove
+        
     """.trimIndent())
 }
-
 fun opcMenuTickets(){
-    print("Selecciona una opció: ")
-    val opcMenu = readln().toInt()
-    when(opcMenu){
-        1 -> println()
-        2 -> println()
-        3 -> println()
-        4 -> println()
-        5 -> println()
-    }
+    var opcMenu: Int
+    do {
+        menuTickets()
+        print("SELECCIONA UNA OPCIÓ: ")
+        if (scanner.hasNextInt()){
+            opcMenu = scanner.nextInt()
+            when(opcMenu){
+                1 -> println()
+                2 -> println()
+                3 -> println()
+                4 -> println()
+                5 -> println()
+            }
+        } else {
+            println("Has d'introduïr una opció del menú")
+            scanner.nextLine()
+            opcMenu = 0
+        }
+
+    }while (opcMenu !in 1..5)
+
 }
+fun zones(): Int{
+    var opcZona: Int
+
+    do {
+        println("""
+            ZONES DE DISTÀNCIA QUE VOL VIATJAR:
+            
+            1   2   3
+            
+        """.trimIndent())
+        print("SELECCIONA UNA OPCIÓ: ")
+        // Verificar si la entrada es un número entero
+        if (scanner.hasNextInt()) {
+            opcZona = scanner.nextInt()
+            // Verificar si la opción es válida
+            when(opcZona){
+                1 -> println("Has escollit $opcZona zona.")
+                2 -> println("Has escollit $opcZona zones.")
+                3 -> println("Has escollit $opcZona zones.")
+            }
+        } else {
+            println("Error! Has de seleccionar una zona. Intenta-ho una altre vegada!.")
+            // Limpiar el buffer del Scanner
+            scanner.nextLine()
+            opcZona = 0 // Valor arbitrario para continuar el bucle
+        }
+
+    } while (opcZona !in 1..3)
+    return opcZona
+}
+
 fun main() {
-    menuTickets()
     opcMenuTickets()
+    zones()
+
 }
